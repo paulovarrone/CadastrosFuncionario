3f1a5b1eefdc805a13dc49892633dbd320a2f2fb
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-12-15 12:06:39 -0300

    atualizações de sessao e posicionamento

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def cadastro_funcinario():
 
     if request.method == 'POST':
         nome = request.form['nome']
-        matricula = request.form['matricula']
+        matricula = request.form['matricula'].strip()
         nascimento = request.form['nascimento']
         contratacao = request.form['contratacao']
         status = request.form['status']
@@ -117,13 +117,121 @@ def cadastro_funcinario():
 
 
     
-@app.route('/alterar', methods=['GET', 'POST'])
-def alterar_dados_cadastrais():
-    return render_template('alter.html')
+# @app.route('/alterar', methods=['GET', 'POST'])
+# def alterar_dados_cadastrais():
+#     return render_template('alter.html')
 
 @app.route('/selecionar', methods=['GET', 'POST'])
 def select_dados_cadastrais():
-    return render_template('select.html')
+    pessoa = None
+
+    if request.method == 'POST':
+        matricula = request.form['matricula'].strip()
+
+        try:
+            conexao = connection()
+            cursor = conexao.cursor(dictionary=True)
+
+            cursor.execute('SELECT * FROM funcionario WHERE matricula = %s', (matricula,))
+            pessoa = cursor.fetchone()
+
+            if not pessoa:
+                flash('Usuário não encontrado no sistema.', 'erro')
+            else:
+                # Salva a matrícula na sessão
+                session['matricula'] = matricula
+
+        except Exception as e:
+            flash(f"Erro {e}", 'erro')
+
+        finally:
+            cursor.close()
+            conexao.close()
+
+        return redirect(url_for('select_dados_cadastrais'))
+
+    # Recupera a matrícula da sessão e busca novamente os dados
+    matricula = session.pop('matricula', None)
+    if matricula:
+        try:
+            conexao = connection()
+            cursor = conexao.cursor(dictionary=True)
+
+            cursor.execute('SELECT * FROM funcionario WHERE matricula = %s', (matricula,))
+            pessoa = cursor.fetchone()
+
+            if pessoa:
+                if pessoa.get('foto'):
+                    pessoa['foto'] = f"data:image/jpeg;base64,{pessoa['foto']}"
+                if pessoa.get('assinatura'):
+                    pessoa['assinatura'] = f"data:image/jpeg;base64,{pessoa['assinatura']}"
+
+        except Exception as e:
+            flash(f"Erro {e}", 'erro')
+
+        finally:
+            cursor.close()
+            conexao.close()
+
+    return render_template('select.html', pessoa=pessoa)
+
+
+
+@app.route('/carteiradigital', methods=['GET', 'POST'])
+def selecionar_e_cadastrar():
+    pessoa = None
+    if request.method == 'POST':
+        matricula = request.form.get('matricula', '').strip()
+
+        # Processo de seleção do funcionário
+        if 'foto' not in request.files:
+            try:
+                conexao = connection()
+                cursor = conexao.cursor(dictionary=True)
+                cursor.execute('SELECT * FROM funcionario WHERE matricula = %s', (matricula,))
+                pessoa = cursor.fetchone()
+                
+                if not pessoa:
+                    flash('Usuário não encontrado no sistema.', 'erro')
+            except Exception as e:
+                flash(f'Erro ao buscar funcionário: {e}', 'erro')
+            finally:
+                cursor.close()
+                conexao.close()
+        else:
+            # Processo de atualização de fotos e assinatura
+            foto = request.files.get('foto')
+            assinatura = request.files.get('assinatura')
+            status = request.form['status']
+
+            if not matricula:
+                flash('Matrícula é obrigatória para atualizar fotos.', 'erro')
+                return redirect(url_for('selecionar_e_cadastrar'))
+
+            foto_b64 = imagem_para_base64(foto)
+            assinatura_b64 = imagem_para_base64(assinatura)
+
+            try:
+                conexao = connection()
+                cursor = conexao.cursor(dictionary=True)
+                cursor.execute("SELECT * FROM funcionario WHERE matricula = %s", (matricula,))
+                pessoa = cursor.fetchone()
+                if pessoa:
+                    query = "UPDATE funcionario SET foto = %s, assinatura = %s, status = %s WHERE matricula = %s"
+                    cursor.execute(query, (foto_b64, assinatura_b64,status, matricula))
+                    conexao.commit()
+                    flash('Foto e assinatura atualizadas com sucesso.', 'sucesso')
+                    return redirect(url_for('selecionar_e_cadastrar'))
+                else:
+                    flash('Usuário não encontrado para atualizar fotos.', 'erro')
+            except Exception as e:
+                flash(f'Erro ao atualizar dados: {e}', 'erro')
+            finally:
+                cursor.close()
+                conexao.close()
+
+      
+    return render_template('carteira.html', pessoa=pessoa)
 
 
 if __name__ == '__main__':
