7af58d7543ce90229dfc89da5344a425bc309a81
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-12-19 19:28:45 -0300

    olho que mostra senha

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -64,3 +64,17 @@ document.querySelector('form').addEventListener('submit', function(event) {
   }
   
 });
+
+function togglePasswordVisibility(inputId, toggleButton) {
+  const passwordInput = document.getElementById(inputId);
+
+  // Alternar o tipo do input
+  if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      toggleButton.innerHTML = '<i class="fas fa-eye-slash"></i>';
+  } else {
+      passwordInput.type = 'password';
+      toggleButton.innerHTML = '<i class="fas fa-eye"></i>';
+  }
+}
+
