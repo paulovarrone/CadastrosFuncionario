0c83749609503470189c24c5685d8eaa245d5acc
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-12-24 22:00:00 -0300

    bi

--- app/controllers/bi/bi.py
+++ app/controllers/bi/bi.py
@@ -11,9 +11,39 @@ def fetch_data_situacao():
     try:
         query = "SELECT nome, matricula, status, contratacao FROM funcionario"
         df_situacao = pd.read_sql(query, engine)
+        
+        # Converter a coluna 'contratacao' para o tipo datetime, lidando com erros
+        df_situacao['contratacao'] = pd.to_datetime(df_situacao['contratacao'], format='%Y-%m-%d', errors='coerce')
+
+        # Substituir valores NaT (datas inválidas) por "Data inválida"
+        df_situacao['contratacao'] = df_situacao['contratacao'].dt.strftime('%d/%m/%Y').fillna("Data inválida")
+        
         return df_situacao
     except Exception as e:
         print(f"Erro ao buscar dados de situação: {e}")
+        raise e
+
+
+def quantidade_funcionarios():
+    try:
+        query = """
+            SELECT status, COUNT(*) AS quantidade FROM funcionario GROUP BY status;
+        """
+        df_quantidade_funcionarios = pd.read_sql(query, engine)
+        return df_quantidade_funcionarios
+    except Exception as e:
+        print(f"Erro ao buscar quantidade de funcionários: {e}")
+        raise e
+
+def quantidade_identificacao_sexual():
+    try:
+        query = """
+            SELECT identificacao_sexual, COUNT(*) AS quantidade FROM funcionario GROUP BY identificacao_sexual;
+        """
+        df_identificacao_sexual = pd.read_sql(query, engine)
+        return df_identificacao_sexual
+    except Exception as e:
+        print(f"Erro ao buscar quantidade de identificação sexual: {e}")
         raise e
 
 def fetch_data_identificacao_sexual():
--- app/routes/powerBI.py
+++ app/routes/powerBI.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, Blueprint
-from app.controllers.bi.bi import fetch_data_situacao, fetch_data_identificacao_sexual, situacao, identificacao_sexual
+from app.controllers.bi.bi import fetch_data_situacao, fetch_data_identificacao_sexual, situacao, identificacao_sexual, quantidade_funcionarios
 from app.decorator.login_required import login_required
 
 bp = Blueprint('powerBI', __name__)
@@ -10,11 +10,16 @@ def powerBI():
     df_situacao = fetch_data_situacao()
     df_identificacao_sexual = fetch_data_identificacao_sexual()
     
+
     filtro = request.args.get('status', '')
 
     situacao_funcionario = situacao()
-    identificacao_sexual_funcionario = identificacao_sexual()
+    # identificacao_sexual_funcionario = identificacao_sexual()
+    qtd_funcionarios = quantidade_funcionarios().to_dict(orient='records')
 
+    total_funcionarios = sum(row['quantidade'] for row in qtd_funcionarios)
+    qtd_funcionarios.append({'status': 'Total', 'quantidade': total_funcionarios})
+    
     if filtro and filtro != 'Todos':
         df_situacao = df_situacao[df_situacao['status'] == filtro]
 
@@ -22,6 +27,7 @@ def powerBI():
     return render_template(
                            'bi.html', tabela = df_situacao.to_dict(orient='records'), 
                            situacao_funcionario = situacao_funcionario, 
-                           identificacao_sexual_funcionario = identificacao_sexual_funcionario, 
-                           filtro = filtro
+                        #    identificacao_sexual_funcionario = identificacao_sexual_funcionario, 
+                           filtro = filtro,
+                           qtd_funcionarios=qtd_funcionarios
                         )
