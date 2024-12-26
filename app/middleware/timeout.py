4c657bb73f1f017d6353267b23a1fe51c2c8f88b
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-12-26 09:35:16 -0300

    auditoria no banco com timeout

--- app/middleware/timeout.py
+++ app/middleware/timeout.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from flask import session, redirect, url_for,flash, current_app
-
+from app.controllers.auditoria.usuario_deslogado.usuario_deslogado import auditoria_banco_deslogado
 
 def timeout():
 
@@ -15,8 +15,9 @@ def timeout():
         last_activity_time = datetime.strptime(last_activity, '%Y-%m-%d %H:%M:%S')
         conta_tempo = datetime.now() - last_activity_time > timeout
 
-        if conta_tempo:
+        if conta_tempo:   
             session.clear()
+            auditoria_banco_deslogado(email, username)
             flash("Sua sessão expirou por inatividade. Faça login novamente.", "erro")
             current_app.logger.info(f"Usuario {username} {email} levou timeout do sistema.")
             return redirect(url_for('login.login'))
